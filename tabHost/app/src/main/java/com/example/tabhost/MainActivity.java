5c75ace1cec6fc9489a60aad1c92d2559444926a
Gairaud <51027552+Gairaud@users.noreply.github.com>
2021-05-24 01:40:56 -0600

    Codigo limpio

--- tabHost/app/src/main/java/com/example/tabhost/MainActivity.java
+++ tabHost/app/src/main/java/com/example/tabhost/MainActivity.java
@@ -16,38 +16,38 @@ public class MainActivity extends TabActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost); // initiate TabHost
-        TabHost.TabSpec spec; // Reusable TabSpec for each tab
-        Intent intent; // Reusable Intent for each tab
+        TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
+        TabHost.TabSpec spec;
+        Intent intent;
 
-        spec = tabHost.newTabSpec("List"); // Create a new TabSpec using tab host
-        spec.setIndicator("List View"); // set the “HOME” as an indicator
-        // Create an Intent to launch an Activity for the tab (to be reused)
+        spec = tabHost.newTabSpec("List");
+        spec.setIndicator("List View");
+
         intent = new Intent(this, ListViewActivity.class);
         spec.setContent(intent);
         tabHost.addTab(spec);
 
-        // Do the same for the other tabs
 
-        spec = tabHost.newTabSpec("Tab Host"); // Create a new TabSpec using tab host
-        spec.setIndicator("Tab Host"); // set the “CONTACT” as an indicator
-        // Create an Intent to launch an Activity for the tab (to be reused)
+
+        spec = tabHost.newTabSpec("Tab Host");
+        spec.setIndicator("Tab Host");
+
         intent = new Intent(this, ContactActivity.class);
         spec.setContent(intent);
         tabHost.addTab(spec);
 
-        spec = tabHost.newTabSpec("Grid view"); // Create a new TabSpec using tab host
-        spec.setIndicator("Grid View"); // set the “ABOUT” as an indicator
-        // Create an Intent to launch an Activity for the tab (to be reused)
+        spec = tabHost.newTabSpec("Grid view");
+        spec.setIndicator("Grid View");
+
         intent = new Intent(this, GridViewActivity.class);
         spec.setContent(intent);
         tabHost.addTab(spec);
-        //set tab which one you want to open first time 0 or 1 or 2
+
         tabHost.setCurrentTab(1);
         tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
             @Override
             public void onTabChanged(String tabId) {
-                // display the name of the tab whenever a tab is changed
+
                 Toast.makeText(getApplicationContext(), tabId, Toast.LENGTH_SHORT).show();
             }
         });
